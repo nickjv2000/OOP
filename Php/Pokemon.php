fc6d4bb28e5202a87379777634fb09d30252e32b
Nick <Nickjv2000@hotmail.nl>
2022-07-01 13:08:24 +0200

    Pokemon.php
    Added health back

--- Php/Pokemon.php
+++ Php/Pokemon.php
@@ -3,17 +3,19 @@ $name;
 $energytype;
 $hitpoints;
 $health;
-$attack;
+$attack1;
+$attack2;
 $weakness;
 $resistance;
 
 class Pokemon {
 
-	public function __construct($name, $energytype, $hitpoints, $attack1, $attack2, $weakness, $resistance)
+	public function __construct($name, $energytype, $hitpoints, $health, $attack1, $attack2, $weakness, $resistance)
 		{
 			$this->name = $name;
 			$this->energytype = $energytype;
 			$this->hitpoints = $hitpoints;
+			$this->health = $health;
 			$this->attack1 = $attack1;
 			$this->attack2 = $attack2;
 			$this->weakness = $weakness;
@@ -30,6 +32,6 @@ class Pokemon {
 	}
 }
 
-$pikachu = new Pokemon('Pika', 'Lightning', '60 hitpoints', 'Electric Ring 50 DMG', 'Pika Punch 20 DMG', 'Fire 1.5x multiplier', 'Fighting 20 points');
+$pikachu = new Pokemon('Pika', 'Lightning', '60 hitpoints', '10 Health', 'Electric Ring 50 DMG', 'Pika Punch 20 DMG', 'Fire 1.5x multiplier', 'Fighting 20 points');
 
-$charmeleon = new Pokemon('Charmy', 'Fire', '60 hitpoints', 'Headbutt 10 DMG', 'Flare 30 DMG', 'Water 2.0x multiplier', 'Lightning 10 points');
+$charmeleon = new Pokemon('Charmy', 'Fire', '60 hitpoints', '10 Health', 'Headbutt 10 DMG', 'Flare 30 DMG', 'Water 2.0x multiplier', 'Lightning 10 points');
