238bc3664bd738e932fd8c5bbfc61387f0bfffdf
Nick <Nickjv2000@hotmail.nl>
2022-09-21 10:43:56 +0200

    Creation classes weakness/resistance/energytype & getter/setter
    Creates all the classes for the weaknessType, resistanceType & energyType.
    
    Changed the values in the pikachu and charmeleon object to only the names they are assigned
    
    Changed the calling of properties of the objects to getter/setter methods

--- Php/pokeman.php
+++ Php/pokeman.php
@@ -1,22 +1,27 @@
 <?php 
-
 include('resistanceType.php');
 include('energyType.php');
 include('weaknessType.php');
 
+// The main Pokemon class to create the properties & constructor to make a pokemon
 class Pokemon{
-	public $name;
-	public $energyType;
-	public $hitpoints;
-	public $health;
-	public $attack1;
-	public $dmg1;
-	public $attack2;
-	public $dmg2;
-	public $weaknessType;
-	public $resistanceType; // encapsulation: what is the right level of visiblity modifiers?
 
-	public function __construct($name, $energyType, $hitpoints, $health, $attack1, $dmg1, $attack2, $dmg2, $weaknessType, $resistanceType)
+	// all the properties with a protected visibility to make the values hidden but useable within the same class or inheratance classes
+	protected $name;
+	protected $energyType;
+	protected $hitpoints;
+	protected $health;
+	protected $attack1;
+	protected $dmg1;
+	protected $attack2;
+	protected $dmg2;
+	protected $weaknessType;
+	protected $resistanceType;
+	protected $worth;
+	protected $multiplier;
+
+	// parent construction function to create a pokemon
+	public function __construct($name, $energyType, $hitpoints, $health, $attack1, $dmg1, $attack2, $dmg2, $weaknessType, $resistanceType, $worth, $multiplier)
 		{
 			$this->name = $name;
 			$this->energyType = $energytype;
@@ -28,29 +33,44 @@ class Pokemon{
 			$this->dmg2 = $dmg2;
 			$this->weaknessType = $weakness;
 			$this->resistanceType = $resistance;
+			$this->worth = $worth;
+			$this->multiplier = $multiplier;
 		}
 
 	public function __toString() {
 		return json_encode($this);
 	}
-}
 
-class Pikachu extends Pokemon{ 
-	
-	public function __construct() {
-		parent::__construct("LightningRat", $energyTypeLightning, 60, 60, "Electric Ring", 50, "Pika Punch", 20, $fireWeakness, $fightingResistance);
+	public function setHitpoints($hitpoints){
+		$this->hitpoints = $hitpoints;
 	}
 
-	// create a Pikachu constructor that calls the parent constructor
-
+	// function to get all the values for the properties to then echo them on the index page
+	public function getName()		{return $this->name;}
+	public function getEnergytype() {return $this->energyType;}
+	public function getHitpoints()  {return $this->hitpoints;}
+	public function getHealth()		{return $this->health;}
+	public function getAttack1()	{return $this->attack1;}
+	public function getDmg1()		{return $this->dmg1;}
+	public function getAttack2()	{return $this->attack2;}
+	public function getDmg2()		{return $this->dmg2;}		
+	public function getWeakness() 	{return $this->weaknessType;}
+	public function getResistance()	{return $this->resistanceType;}
+	public function getWorth()		{return $this->worth;}
+	public function getMultiplier()	{return $this->multiplier;}
 }
 
+
+// Pikachu class to create a Pikachu object
+class Pikachu extends Pokemon{ 	
+	public function __construct($name) {
+		parent::__construct($name, $energyTypeLightning, 60, 60, "Electric Ring", 50, "Pika Punch", 20, $fireWeakness, $fightingResistance, 20, 1.5);
+	}
+}
+
+// Charmeleon class to create a Charmeleon object
 class Charmeleon extends Pokemon{
-		public function __construct() {
-		parent::__construct('FireMonster', $energyTypeFire, 60, 60, 'Headbutt', 10, 'Flare', 30, $waterResistance, $lightningWeakness);
+		public function __construct($name) {
+		parent::__construct($name, $energyTypeFire, 60, 60, "Headbutt", 10, "Flare", 30, $waterWeakness, $lightningResistance, 10, 2);
 	}
 }
-
-$pikachu = new Pikachu();
-
-$charmeleon = new Charmeleon();
