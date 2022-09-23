185a6c07789dded458763340498fc4ebcf04e7bf
Nick <Nickjv2000@hotmail.nl>
2022-09-23 11:26:03 +0200

    Pikachu.php attacks WIP
    Added the proper way of calling the attacks on pikachu. Subclass can't read it yet and thus WIP

--- Php/pikachu.php
+++ Php/pikachu.php
@@ -2,7 +2,7 @@
 // Pikachu class to create a Pikachu object
 class Pikachu extends Pokemon{ 	
 	public function __construct($name) {
-		parent::__construct($name, $energyTypeLightning, 60, 60, "Electric Ring", 50, "Pika Punch", 20, $fireWeakness, $fightingResistance, 20, 1.5);
+		parent::__construct($name, $energyTypeLightning, 60, 60, $electricRing, $pikaPunch, $fireWeakness, $fightingResistance, 20, 1.5);
 	}
 }
 
