e7a54fdaeb1440fd8034c7334072f4ee4e40aef3
Nick <Nickjv2000@hotmail.nl>
2022-09-23 13:56:24 +0200

    pikachu & charmeleon object properties
    filling out both the subclass & parent constructor with the properties rather than the values because pure values didn't work.

--- Php/charmeleon.php
+++ Php/charmeleon.php
@@ -2,8 +2,8 @@
 
 // Charmeleon class to create a Charmeleon object
 class Charmeleon extends Pokemon{
-		public function __construct($name) {
-		parent::__construct($name, $energyTypeFire, 60, 60, "Headbutt", 10, "Flare", 30, $waterWeakness, $lightningResistance, 10, 2);
+		public function __construct($name, $energyType, $hitpoints, $health, $attack1, $attack2, $weaknessType, $resistanceType, $worth, $multiplier) {
+		parent::__construct($name, $energyType, $hitpoints, $health, $attack1, $attack2, $weaknessType, $resistanceType, $worth, $multiplier);
 	}
 }
 
--- Php/pikachu.php
+++ Php/pikachu.php
@@ -1,9 +1,8 @@
 <?php 
-// Pikachu class to create a Pikachu object
+// Pikachu class extending pokemon to create an object
 class Pikachu extends Pokemon{ 	
-	public function __construct($name) {
-		parent::__construct($name, $energyTypeLightning, 60, 60, $electricRing, $pikaPunch, $fireWeakness, $fightingResistance, 20, 1.5);
+	public function __construct($name, $energyType, $hitpoints, $health, $attack1, $attack2, $weaknessType, $resistanceType, $worth, $multiplier) {
+		parent::__construct($name, $energyType, $hitpoints, $health, $attack1, $attack2, $weaknessType, $resistanceType, $worth, $multiplier);
 	}
 }
-
 ?>
